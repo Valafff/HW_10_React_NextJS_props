eee0a72ad82f88cd92fdadd73603c164d7b14125
Vetal <Vetal@GB550PRO>
2025-03-01 18:46:32 +0300

    up

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,92 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Image from 'next/image';
+
+function StudentID(props) {
+  //props
+  let 
+  {
+    number,
+    lastName,
+    firstName,
+    middleName,
+    faculty,
+    studyForm,
+    orderNumber,
+    enterDate,
+    date,
+    year,
+    month,
+    homerSignature,
+    rectorName,
+    rectorSignature,
+    backColor
+  } = props;
+
+  let ticketStyles = 
+  {
+    backgroundColor: backColor,
+    border: "2px solid black",
+    padding: "20px",
+    maxWidth: "500px",
+    margin: "0 auto",
+    textAlign: "left",
+  };
+
+  return (
+    <div style={ticketStyles}>
+      <h1 style={{ textAlign: "center" }}>СТУДЕНЧЕСКИЙ БИЛЕТ № {number}</h1>
+      <p>Фамилия: {lastName}</p>
+      <p>Имя: {firstName}</p>
+      <p>Отчество: {middleName}</p>
+      <p>Факультет: {faculty}</p>
+      <p>Форма обучения: {studyForm}</p>
+      <p>Зачислен приказом от " {enterDate} " {month} 20{year} г. № {orderNumber}</p>
+      <p>Дата выдачи: " {date} " {month} 20{year} г.</p>
+      <p>Подпись студента: {homerSignature}</p>
+      <div style={{
+        width: "150px",
+        height: "200px",
+        border: "1px dashed #000",
+        margin: "20px 0",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        overflow: "hidden", // Чтобы изображение не выходило за границы
+      }}>
+        <Image
+          src="/homer.png" 
+          alt="Фото студента"
+          width={150}
+          height={200}
+          style={{ objectFit: "cover" }} // Чтобы изображение заполниило область
+        />
+      </div>
+      <p><strong>Ректор:</strong> {rectorName}</p>
+      <p>(подпись: {rectorSignature})</p>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <StudentID
+        number = {1234567890}
+        lastName="Симсон"
+        firstName="Гомер"
+        middleName="Джей"
+        faculty="Атомная физика"
+        studyForm="Очная"
+        orderNumber="123USA"
+        enterDate="1"
+        date="1"
+        year = "25"
+        month = "марта"
+        homerSignature ="HJS"
+        rectorName="Сидоров И.П."
+        rectorSignature = "СиДоРоВ"
+        backColor="lightgrey"
+      />
     </div>
   );
 }
